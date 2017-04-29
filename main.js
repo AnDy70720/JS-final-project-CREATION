6bc46e63d62dcc7a6afabaab52a1969346a4b481
AnDy70720 <tsps99154@student.tsps.tp.edu.tw>
2017-04-29 15:20:30 +0800

    draw the background

---
+++ main.js
@@ -0,0 +1,8 @@
+var bgImg = document.createElement('img');
+bgIlmg.src = 'images/map.png';
+var canvas =  document.getElementBylId('game-canvas');
+var ctx = canvas.getContext('2d');
+function draw(){
+	ctx.drawImage(bgImg,0,0);
+}
+draw();
