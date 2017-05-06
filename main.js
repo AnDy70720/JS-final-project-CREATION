67426248347f4f2a5519f2954c7bec49e1c7d428
AnDy70720 <tsps99154@student.tsps.tp.edu.tw>
2017-05-06 13:50:59 +0800

    真真的能是背景跑出來

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var bgImg = document.createElement('img');
 bgImg.src = 'images/map.png';
-var canvas =  document.getElementBylId('game-canvas');
+var canvas =  document.getElementById('game-canvas');
 var ctx = canvas.getContext('2d');
 function draw(){
 	ctx.drawImage(bgImg,0,0);
