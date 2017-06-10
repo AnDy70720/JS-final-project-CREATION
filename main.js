5cfc2424ecdfa27bd068c9047d13011a7a41df27
AnDy70720 <tsps99154@student.tsps.tp.edu.tw>
2017-06-10 15:28:54 +0800

    敵軍大量出沒！！

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var bgImg = document.createElement("img");
-bgImg.src = "images/map.png";
+bgImg.src = "images/map.bmp";
 
 
 var rukiaImg = document.createElement("img");
@@ -13,34 +13,59 @@ towerImg.src = "images/tower.png";
  
 var fps = 60
 
-var rukia ={
-	x:96,
-	y:448,
-	speed:64,
-	direction:{
+var enemyPath = [
+    {x:96, y:64},
+    {x:384, y:64},
+    {x:384, y:192},
+    {x:224, y:192},
+    {x:224, y:320},
+    {x:544, y:320},
+    {x:544, y:64},
+	{/*none*/}
+
+];
+function Rukia(){
+	this.x=96;
+	this.y=448;
+	this.speed=64;
+	this.direction={
 		x:0,
 		y:-1
 	},
-	move:function(){
-		this.x = this.x + this.direction.x * this.speed/fps;
-		this.y = this.y + this.direction.y * this.speed/fps;
+	this.pathDes=0,
+	this.move=function(){
+		if(isCollided(enemyPath[this.pathDes].x, enemyPath[this.pathDes].y, this.x, this.y, this.speed/fps, this.speed/fps)){
+			this.x = enemyPath[this.pathDes].x;
+        	this.y = enemyPath[this.pathDes].y;
+        	this.pathDes++;
+        	var unitVector = getUnitVector(this.x, this.y, enemyPath[this.pathDes].x, enemyPath[this.pathDes].y);
+        	this.direction = unitVector;
+		}else{
+			this.x = this.x + this.direction.x * this.speed/fps;
+			this.y = this.y + this.direction.y * this.speed/fps;
+		}
 	}
 }
-var tower={
+
+var tower = {
 	x:0,
 	y:0
-}
+};
 
 var isbiding = false;
 
+var enemies = [];
+var clock = 0;
+var click=false;
 
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext('2d');
 
 $('#game-canvas').mousemove(function(event){
-
+if(click==false){
 	tower.x = event.offsetX;
 	tower.y = event.offsetY;
+	}
 })
  
 
@@ -51,24 +76,56 @@ $('#game-canvas').click(function(){
 		}else{
 			isbiding = false;
 		}
+	}else{
+		click = true;
 	}
 })
 
 
 function draw(){
+	clock++;
+	if(clock%80==0){
+		var newenemy = new Rukia();
+		enemies.push(newenemy);
+	}
 	ctx.drawImage(bgImg,0,0);
-	ctx.drawImage(rukiaImg,rukia.x,rukia.y);
+
+	for(var i=0;i<enemies.length;i++){
+		ctx.drawImage(rukiaImg,enemies[i].x,enemies[i].y);
+		enemies[i].move();
+	}
+
+	
 	ctx.drawImage(towerBtnImg,640-64,480-64,64,64);
 	if(isbiding == true){
 		ctx.drawImage(towerImg,tower.x-tower.x%32,tower.y-tower.y%32);
 	}
-	rukia.move();
 }
 
+function isCollided(pointX, pointY, targetX, targetY, targetWidth, targetHeight) {
+    if(     pointX >= targetX
+        &&  pointX <= targetX + targetWidth
+        &&  pointY >= targetY
+        &&  pointY <= targetY + targetHeight
+    ){
+        return true;
+    } else {
+        return false;
+    }
+}
+function getUnitVector(srcX, srcY, targetX, targetY){
+	var offsetX = targetX - srcX;
+	var offsetY = targetY - srcY;
+	var distance = Math.sqrt(Math.pow(offsetX,2) + Math.pow(offsetY,2));
+	var unitVector = {
+		x:offsetX/distance,
+		y:offsetY/distance
+	};
+	return unitVector;
+};
 
 
-
-setInterval(draw,16);
+setInterval(draw,1000/fps);
 
 
 
